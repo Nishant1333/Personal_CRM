d2693879c7ebd4f88ddfb4cfc8241bd793fa44df
Nishant Bhardwaj <126180802+Nishant1333@users.noreply.github.com>
2024-05-18 13:51:54 +0000

    file added

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Button from './conponents/Button'
+ import './App.css'
+function App() {
+
+
+  return (
+    <>
+      <div className='button-container'>
+        <Button />
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
